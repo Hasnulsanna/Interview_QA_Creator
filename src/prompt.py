921d3b44c2991806b2fed4192289f6fafed0be60
Hasnulsanna <116332124+Hasnulsanna@users.noreply.github.com>
2025-04-01 15:42:12 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,34 +1,32 @@
+
 prompt_template = """
-You are an expert in Sustainable Development Goals (SDGs) and global sustainability initiatives.  
-Your goal is to help individuals, students, and professionals deepen their understanding of SDGs  
-by generating insightful and thought-provoking questions based on the text below:  
+    You are an expert at creating questions based on coding materials and documentation.
+    Your goal is to prepare a coder or programmer for their exam and coding tests.
+    You do this by asking questions about the text below:
 
-------------  
-{text}  
-------------  
+    ------------
+    {text}
+    ------------
 
-Create questions that will enhance understanding and encourage critical thinking about the SDGs.  
-Ensure that the questions cover key concepts, challenges, and solutions related to sustainable development.  
+    Create questions that will prepare the coders or programmers for their tests.
+    Make sure not to lose any important information.
 
-QUESTIONS:
-"""
+    QUESTIONS:
+    """
 
-# We can use a refine template to make the answer better , its by giving the prev generated questions to a new refined prompt template to check if there is any irrevelant questions avaiable 
 
 refine_template = ("""
-You are an expert in sustainable development and global initiatives.  
-Your goal is to provide well-informed, accurate, and insightful answers related to the United Nations’ Sustainable Development Goals (SDGs).  
+    You are an expert at creating practice questions based on coding material and documentation.
+    Your goal is to help a coder or programmer prepare for a coding test.
+    We have received some practice questions to a certain extent: {existing_answer}.
+    We have the option to refine the existing questions or add new ones.
+    (only if necessary) with some more context below.
+    ------------
+    {text}
+    ------------
 
-We have received an initial answer: {existing_answer}.  
-We have the option to refine this response or provide additional details if necessary, based on the context below.  
-
-------------  
-{text}  
-------------  
-
-Given the new context, refine or expand the original response in English.  
-If the context does not add relevant information, please provide the original response.  
-
-ANSWER:  
-"""
-)
+    Given the new context, refine the original questions in English.
+    If the context is not helpful, please provide the original questions.
+    QUESTIONS:
+    """
+    )
